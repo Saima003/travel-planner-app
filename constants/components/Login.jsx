2d126e7bc4b90ac593f542eeaf15459910542144
Saima Khan <saima19@somaiya.edu>
2024-10-23 22:05:06 +0530

    Feat: Bottom sheet for login and signup

--- constants/components/Login.jsx
+++ constants/components/Login.jsx
@@ -1,14 +1,34 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const Login = () => {
+  const sheetRef = useRef(null);
+  // const [isOpen, setIsOpen] = useState(true);
+  const snapPoints = ["40%","60%"];
+
   return (
-    <View>
-      <Image source={require("../../assets/images/WhatsApp Image 2024-10-16 at 9.08.47 PM.jpg")} style={{width: "100%", height:"80%"}}/>
-    </View>
-  )
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Image 
+          source={require("../../assets/images/WhatsApp Image 2024-10-16 at 9.08.47 PM.jpg")} 
+          style={{ width: "100%", height: "100%" }} 
+        />
+        <BottomSheet
+          ref={sheetRef}
+          snapPoints={snapPoints}
+          enablePanDownToClose={false}
+        >
+          <BottomSheetView>
+          <Text style={{textAlign:"center"}}>Login Gaizzz</Text>
+          </BottomSheetView>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
+  );
 }
 
-export default Login
+export default Login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
