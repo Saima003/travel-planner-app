15d96b90d730885a92d7393ce7cd13c0d0b3c821
Saima Khan <saima19@somaiya.edu>
2024-10-27 16:59:28 +0530

    Login layout and font families

--- constants/components/Login.jsx
+++ constants/components/Login.jsx
@@ -1,13 +1,17 @@
 import { Image, StyleSheet, Text, View } from 'react-native';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Colors } from '../Colors';
 
 const Login = () => {
   const sheetRef = useRef(null);
   // const [isOpen, setIsOpen] = useState(true);
   const snapPoints = ["40%","60%"];
-
+  useEffect(() => {
+    // Open the bottom sheet when the component mounts
+    sheetRef.current?.snapToIndex(0);
+  }, []);
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
@@ -21,7 +25,11 @@ const Login = () => {
           enablePanDownToClose={false}
         >
           <BottomSheetView>
-          <Text style={{textAlign:"center"}}>Login Gaizzz</Text>
+          <Text style={{textAlign:"center", fontFamily:"outfit-bold", fontSize:28}}>AI Travel Planner</Text>
+          <Text style={{textAlign:"center", fontFamily:"outfit", fontSize:18, color:Colors.GRAY}}>Discover your next adventure effortlessly.Personalized itineraries at your fingertips.Travel smarter with AI-driven insights.</Text>
+          <View style={styles.button}>
+            <Text style={{color:Colors.WHITE, textAlign:"center", fontFamily:"outfit", fontSize:17}}>Sign In With Google</Text>
+          </View>
           </BottomSheetView>
         </BottomSheet>
       </View>
@@ -31,4 +39,12 @@ const Login = () => {
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button:{
+    padding:10,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius:99,
+    marginVertical:"20%",
+    marginHorizontal:"5%"
+  }
+});
