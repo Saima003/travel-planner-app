ea3e91d042466016aa3476c83a303db8ec6193aa
Saima Khan <saima19@somaiya.edu>
2025-02-03 22:52:33 +0530

    Integration of hotel info (in progress)

--- components/TripPlan/HotelInfo.jsx
+++ components/TripPlan/HotelInfo.jsx
@@ -1,18 +1,22 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const HotelInfo = ({hotelInfo}) => {
-  return (
-    <View style={{marginTop:20}}>
-      <Text style={{fontFamily:'outfit-bold', fontSize:17}}>🏨 Hotel Recommendations</Text>
-      <FlatList data={hotelInfo} renderItem={(item, index)=>(
-        <View> 
-            {console.log(item,"item")}
-            <Text>{item?.item?.geo_coordinates?.hotel_name}</Text>
+const HotelInfo = ({ hotelInfo }) => {
+    return (
+        <View style={{ marginTop: 20 }}>
+            <Text style={{ fontFamily: 'outfit-bold', fontSize: 17 }}>🏨 Hotel Recommendations</Text>
+            <FlatList data={hotelInfo} renderItem={(item, index) => (
+                <View>
+                    <Image
+                        source={{ uri: item?.item?.image_url }}
+                        style={{ width: "100%", height: 240, objectFit: "cover", borderRadius: 15 }}
+                        resizeMode="contain"
+                    />
+                    <Text>{item?.item?.hotel_name}</Text>
+                </View>
+            )} />
         </View>
-      )}/>
-    </View>
-  )
+    )
 }
 
 export default HotelInfo
