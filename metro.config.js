08a24415052a2eecb03d7f4f79ae6f5b84e8bd47
Saima Khan <saima19@somaiya.edu>
2025-03-02 19:36:21 +0530

    Dependencies and build issues fixed

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,18 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Enable symlinks (useful for monorepos)
+config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
+
+// Support .cjs and .mjs files
+config.resolver.sourceExts.push('cjs', 'mjs');
+
+// Improve performance with inline requires
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+  inlineRequires: true,
+};
 
 module.exports = config;
